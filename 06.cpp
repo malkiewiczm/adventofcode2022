34f99269d0c3b7f0be0eacd60f911e4cae1fc2b7
malkiewiczm <malkiewiczm@users.noreply.github.com>
2022-12-06 00:12:29 -0500

    Tue, Dec  6, 2022 00:12:29

---
+++ 06.cpp
@@ -0,0 +1,59 @@
+//#define PARTONE
+#include "template.hpp"
+
+#ifdef PARTONE
+
+static bool check_four(const std::string &s, int i)
+{
+	FOR(a, 4) {
+		FOR2(b, a + 1, 4) {
+			if (s[i + a] == s[i + b])
+				return false;
+		}
+	}
+	return true;
+}
+
+static int lcount(const std::string &s)
+{
+	FOR(i, s.size() - 3) {
+		if (check_four(s, i))
+			return i + 4;
+	}
+	return 0;
+}
+
+#else
+
+static bool check_fourteen(const std::string &s, int i)
+{
+	std::set<char> u;
+	FOR(a, 14) {
+		u.emplace(s[i + a]);
+	}
+	return u.size() == 14;
+}
+
+static int lcount(const std::string &s)
+{
+	FOR(i, s.size() - 13) {
+		if (check_fourteen(s, i))
+			return i + 14;
+	}
+	return 0;
+}
+
+#endif
+
+int main(int argc, char **argv)
+{
+	const char *const fname = (argc >= 2) ? argv[1] : "in.txt";
+	std::ifstream f(fname);
+	if (! f.good()) {
+		TRACE << "file cannot be opened" << std::endl;
+		die();
+	}
+	std::string line;
+	std::getline(f, line);
+	trace(lcount(line));
+}
--- template.hpp
+++ template.hpp
@@ -5,6 +5,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <limits>
